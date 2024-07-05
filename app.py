a1ee37c9e9ecbd0f9e7d44a427ddfe1f282f50b0
Lohith <69358508+lohithgsk@users.noreply.github.com>
2024-07-06 02:25:16 +0530

    added analysis

--- app.py
+++ app.py
@@ -16,3 +16,28 @@ def home():
 def analysis():
     if (request.method == "GET"):
         return render_template("analysis.html")
+    
+@app.route("/analysis/articulated_truck", methods=["GET"])
+def articulated_truck():
+    if (request.method == "GET"):
+        return render_template("articulated_truck.html")
+    
+@app.route("/analysis/asphalt_paver", methods=["GET"])
+def asphalt_paver():
+    if (request.method == "GET"):
+        return render_template("asphalt_paver.html")
+
+@app.route("/analysis/backhoe_loader", methods=["GET"])
+def backhoe_loader():
+    if (request.method == "GET"):
+        return render_template("backhoe_loader.html")
+    
+@app.route("/analysis/dozer", methods=["GET"])
+def dozer():
+    if (request.method == "GET"):
+        return render_template("dozer.html")
+    
+@app.route("/analysis/excavator", methods=["GET"])
+def excavator():
+    if (request.method == "GET"):
+        return render_template("excavator.html")
